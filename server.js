12ebc5a8970437cbfc173de5ada4d3000be62262
B.Ashita <battua@iitbhilai.ac.in>
2021-06-24 00:21:34 +0530

    Created the basic frontpage UI

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.listen(PORT, function(err) {
-    if(err) {
+app.use(express.static('frontend'));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + "/frontend/frontPage.html"));
+});
+
+app.listen(PORT, function (err) {
+    if (err) {
         console.log("There is an error in setting up the server");
     }
     console.log("Server listening to port" + "" + ":" + PORT);
-})
-
+});
 
