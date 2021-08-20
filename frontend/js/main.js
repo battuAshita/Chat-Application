fcb8a94a9c0b6e09a5fad6fbc03e31671933b1f7
B.Ashita <battua@iitbhilai.ac.in>
2021-08-20 17:28:23 +0530

    Craeted the UI for the chat window

---
+++ frontend/js/main.js
@@ -0,0 +1,5 @@
+function io() {
+    return undefined;
+}
+
+const socket = io();
--- server.js
+++ server.js
@@ -1,18 +1,30 @@
 const path = require('path');
 const express = require('express');
-const app = express();
-const PORT = 3000;
+const socket = require('socket.io');
 
+//Setup the app
+const PORT = 3000;
+const app = express();
+const server = app.listen(PORT, function (err) {
+    if (err) {
+        console.log("There is an error in setting up the server");
+    }
+    console.log("Server listening to port" + "" + ":" + PORT);
+});
+
+//Setup the socket
+const io = socket(server);
+
+//Set the static folder
 app.use(express.static('frontend'));
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + "/frontend/frontPage.html"));
 });
 
-app.listen(PORT, function (err) {
-    if (err) {
-        console.log("There is an error in setting up the server");
-    }
-    console.log("Server listening to port" + "" + ":" + PORT);
-});
+io.on('connection', function (socket) {
+    console.log("New websocket connection...");
+})
+
+
 
