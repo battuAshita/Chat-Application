97b017cf70a650718fbc8c60e94df8e7bca523b0
B.Ashita <battua@iitbhilai.ac.in>
2021-08-25 19:12:06 +0530

    Added the functionality of dispalying the message on UI

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,5 +1,48 @@
-function io() {
-    return undefined;
-}
-
+const chatForm = document.getElementById('chat-form');
+const chatWindow = document.querySelector('.chat-messages');
 const socket = io();
+
+//Get username
+const username = Qs.parse(location.search, {
+    ignoreQueryPrefix: true
+})
+
+//Join chatroom
+
+
+//Capture the message sent
+socket.on('message', function(message) {
+    console.log(message);
+    displayMessage(message);
+
+    //Scroll down feature for the chat window
+    chatWindow.scrollTop = chatWindow.scrollHeight;
+});
+
+chatForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    //Get the message
+    const msg = e.target.elements.msg.value;
+
+    //Send message to the server
+    socket.emit('message', msg);
+
+    //Clear the typing space after sending message
+    e.target.elements.msg.value = '';
+    e.target.elements.msg.focus();
+
+});
+
+//Display the message on the chat screen
+function displayMessage(message) {
+    const newDiv = document.createElement('newDiv');
+    newDiv.classList.add('message');
+    newDiv.innerHTML = `<p class="meta">${message.username} &nbsp<span>${message.time}</span></p> 
+    <p class="text">
+        ${message.text}
+    </p>`;
+
+    document.querySelector('.chat-messages').appendChild(newDiv);
+
+}
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const express = require('express');
-const socket = require('socket.io');
+const socketio = require('socket.io');
+
+const formatMessage = require('./utils/messages');
 
 //Setup the app
 const PORT = 3000;
@@ -13,18 +15,35 @@ const server = app.listen(PORT, function (err) {
 });
 
 //Setup the socket
-const io = socket(server);
+const io = socketio(server);
 
 //Set the static folder
 app.use(express.static('frontend'));
 
+//Get request to the frontpage
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + "/frontend/frontPage.html"));
 });
 
 io.on('connection', function (socket) {
     console.log("New websocket connection...");
-})
 
+    socket.emit('message', formatMessage('Chat Bot', 'Welcome to my chat application!'));
+
+    //Notify when a user connects
+    socket.broadcast.emit('message', formatMessage('Chat Bot', 'A user has joined the room'));
+
+    //Client disconnects
+    socket.on('disconnect', function() {
+        io.emit('message', formatMessage('Chat Bot', 'A user has left the room'));
+    });
+
+    //Capture the message from the client
+    socket.on('message', function(message) {
+        io.emit('message', formatMessage('user', message));
+    });
+    
+});
+  
 
 
